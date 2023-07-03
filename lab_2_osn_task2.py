f58998d138638cd4f4c04c39e8ffdd2c58476e23
Melanikot <137421964+Melanikot@users.noreply.github.com>
2023-07-03 15:49:57 +0000

    lab_2

---
+++ lab_2_osn_task2.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = int(input())
+c = int(input())
+i = a
+while i <= c:
+    print(i)
+    i *= b
---
+++ lab_2_osn_task4.py
@@ -0,0 +1,12 @@
+a = int(input('Введите натуральное число: '))
+if a == 0:
+    print('Ошибка')
+b = 1
+c = 0
+d = b
+i = 0
+while i <= a:
+    print (d)
+    c = b
+    i += 1
+    d1 += b + c and b = d
