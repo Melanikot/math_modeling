15c14c9ea3d938f35565a02d3c7c908fd5682600
Melanikot <137421964+Melanikot@users.noreply.github.com>
2023-07-06 12:05:57 +0000

    lab 2

--- lab_2_dop_task4.py
+++ lab_2_dop_task4.py
@@ -2,5 +2,15 @@ a = int(input('Введите натуральное число: '))
 if a <= 0:
     print('Ошибка')
 else:
-    b = a % i
+    b = []
+    for i in range(1, a + 1, 1):
+        if a % i == 0:
+            b.append(i)
+    print(b)
+    if b[1] == a:
+        print('a =', a)
+    else:
+        for x in range
+
+
     
--- lab_2_dop_task5.py
+++ lab_2_dop_task5.py
@@ -2,4 +2,16 @@ a = int(input('Введите натуральное число: '))
 if a <= 0:
     print('Ошибка')
 else:
+    b = []
+    c = 0
+    g = []
+    for y in range(1, a + 1, 1):
+        for i in range(1, y, 1):
+            if y % i == 0:
+                b.append(i)
+        for x in b:
+            c += x
+        if c == y:
+            g.append(y)
+    print(g)
     
