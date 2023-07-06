08ab69982ac1bc122c522fd23ddf6f3342bb253a
Melanikot <137421964+Melanikot@users.noreply.github.com>
2023-07-06 14:15:06 +0000

    lab 2

--- lab_2_dop_task5.py
+++ lab_2_dop_task5.py
@@ -6,11 +6,15 @@ else:
     c = 0
     g = []
     for y in range(1, a + 1, 1):
+        print(f'Зашли сюда {y}')
         for i in range(1, y, 1):
+            print(f'Зашли сюда {i}')
             if y % i == 0:
                 b.append(i)
+                print(f'Зашли сюда {b}')
         for x in b:
             c += x
+            print(f'Зашли сюда {c}')
         if c == y:
             g.append(y)
     print(g)
