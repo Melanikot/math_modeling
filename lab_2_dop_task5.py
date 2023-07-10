491712f74322a1285d2318dac3ce685dcc706fb5
Melanikot <137421964+Melanikot@users.noreply.github.com>
2023-07-10 08:53:54 +0000

    lab 2

--- lab_2_dop_task5.py
+++ lab_2_dop_task5.py
@@ -2,20 +2,16 @@ a = int(input('Введите натуральное число: '))
 if a <= 0:
     print('Ошибка')
 else:
-    b = []
-    c = 0
-    g = []
+    
     for y in range(1, a + 1, 1):
-        print(f'Зашли сюда {y}')
+        c = 0
+        b = []
         for i in range(1, y, 1):
-            print(f'Зашли сюда {i}')
             if y % i == 0:
                 b.append(i)
-                print(f'Зашли сюда {b}')
         for x in b:
             c += x
-            print(f'Зашли сюда {c}')
         if c == y:
-            g.append(y)
-    print(g)
+            print(y)
+
     
