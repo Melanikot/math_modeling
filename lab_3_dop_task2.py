9d97165088cc68df415fc4003959387b0cbc3bb7
Melanikot <137421964+Melanikot@users.noreply.github.com>
2023-07-12 10:15:20 +0000

    lab_3

--- lab_3_dop_task2.py
+++ lab_3_dop_task2.py
@@ -1,11 +1,12 @@
 import numpy as np
 a = np.zeros((1, 5))
-for i in range(0, 4, 1):
+for i in range(0, 5, 1):
     a[0, i] = int(input(f'Введите значение {i + 1}-го элемента: '))
+a[0, 4] = 0
+print(a)
 b = int(input('Введите число: '))
 c = int(input('Введите номер позиции: '))
-for j in range(c, 4, 1):
+for j in range(4, c - 1, -1):
     a[0, j] = a[0, j - 1]
-a[0, 4] = a[0, 3]
 a[0, c - 1] = b
 print(a)
