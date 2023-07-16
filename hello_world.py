d990e1d00676866293e8e40025be6d79fec14e06
Melanikot <137421964+Melanikot@users.noreply.github.com>
2023-07-16 11:11:27 +0000

    -

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,1 @@
 print('hello world')
-Plank_constant = h = 6.62607015 * 10 ** (-34)
-gravitational_constant = G = 6.67384 * 10 ** (-11)
-acceleration_of_gravity = g = 9.80665
-Stefan_Boltzmann_constant = o = 5.670373 * 10 ** (-8)
-Boltzmann_constant = k = 1.380649 * 10 ** (-23)
-gas_constant = R = 8.3145
-speed_of_light = c = 299792458
-Hubble_constant = H = 68
